6b34d24cc3e2c3687cb5bdba6961429eb3131af7
AceOfKestrels <aceofkestrels@gmail.com>
2024-12-12 11:04:47 +0100

    Ask all questions as free form

--- prompt/Prompt.go
+++ prompt/Prompt.go
@@ -2,30 +2,10 @@ package prompt
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 )
 
-const promptMultipleChoice string = "Welche Antwort ist die richtige? Gib nur die richtige Zahl bzw den richtigen Buchstaben.\n\n"
-const promptOpen string = "Beantworte die folgende Frage in einem kurzen Stichpunkt.\n\n"
+const prompt string = "Beantworte die folgende Frage in einem kurzen Stichpunkt.\n\n"
 
 func New(question string) string {
-	var prompt string
-	if isMultipleChoice(question) {
-		prompt = promptMultipleChoice + question
-	} else {
-		prompt = promptOpen + question
-	}
-	return fmt.Sprintf("{ \"contents\": [ { \"parts\": [ {\"text\": \"%v\" } ] } ] }", prompt)
-}
-
-func isMultipleChoice(question string) bool {
-	if !strings.Contains(question, "?") {
-		return false
-	}
-
-	optionPattern := regexp.MustCompile(`(?m)^\s*([A-Z]\.|[1-9]\.|\d\))\s+`)
-	options := optionPattern.FindAllStringSubmatch(question, -1)
-
-	return len(options) >= 2
+	return fmt.Sprintf("{ \"contents\": [ { \"parts\": [ {\"text\": \"%v\" } ] } ] }", prompt+question)
 }
