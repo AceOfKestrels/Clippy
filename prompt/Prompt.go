389de82f7e70da68af116ad240ae675577c1bc63
AceOfKestrels <aceofkestrels@gmail.com>
2024-12-19 11:56:30 +0100

    Fix unescaped characters in question

--- prompt/Prompt.go
+++ prompt/Prompt.go
@@ -1,11 +1,20 @@
 package prompt
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 const prompt string = "Beantworte die folgende Frage in einem kurzen Stichpunkt.\n\n"
 
 func New(question string) string {
-	return fmt.Sprintf("{ \"contents\": [ { \"parts\": [ {\"text\": \"%v\" } ] } ] }", prompt+question)
+	return fmt.Sprintf("{ \"contents\": [ { \"parts\": [ {\"text\": \"%v\" } ] } ] }", escapeJsonCharacters(prompt+question))
+}
+
+func escapeJsonCharacters(str string) string {
+	escaped, _ := json.Marshal(str)
+	escapedString, _ := strings.CutPrefix(string(escaped), "\"")
+	escapedString, _ = strings.CutSuffix(escapedString, "\"")
+	return escapedString
 }
